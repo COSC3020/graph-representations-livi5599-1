9710fb3e34649439549637821e2b1fb5e5c4dc7e
livi5599 <obrown1@uwyo.edu>
2025-01-25 17:07:00 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,27 +4,22 @@ const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 const test =
-    jsc.forall("array (pair nat nat)", function(edges) {
-        var max = edges.reduce(function(a, b) { return Math.max(a, Math.max(b[0], b[1])); }, 0);
-        var mat = [];
+    jsc.forall("array (array nat)", function(list) {
+        list = list.map(sublist =>
+            sublist.filter(neighbor => neighbor >= 0 && neighbor < list.length)
+        );
+        
+        var max = list.length - 1;
+        var expectedMatrix = [];
         for(var i = 0; i <= max; i++) {
-            mat[i] = [];
+            expectedMatrix[i] = [];
             for(var j = 0; j <= max; j++) {
-                mat[i][j] = 0;
+                expectedMatrix[i][j] = list[i] && list[i].includes(j) ? 1:0;
             }
-            for(var j = 0; j < edges.length; j++) {
-                if(edges[j][0] == i) mat[i][edges[j][1]] = 1;
-            }
-        }
-        var list = [];
-        for(var i = 0; i <= max; i++) {
-            list[i] = [];
-            for(var j = 0; j < edges.length; j++) {
-                if(edges[j][0] == i) list[i].push(edges[j][1]);
-            }
+
             list[i].sort(function(a, b) { return a - b; });
             list[i] = [...new Set(list[i])];
         }
-        return JSON.stringify(list) == JSON.stringify(convertToAdjList(mat));
+        return JSON.stringify(expectedMatrix) == JSON.stringify(convertToAdjMatrix(list));
     });
 jsc.assert(test, { tests: 1000 });
